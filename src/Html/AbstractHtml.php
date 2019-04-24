24f13e7f7a9e3032c4faef265cc2a9525dc7ef43
亀井亜佐夫 <asao.kamei@gmail.com>
2019-04-24 15:22:56 +0900

    writing up a code to generate html code.

---
+++ src/Html/AbstractHtml.php
@@ -0,0 +1,135 @@
+<?php
+namespace WScore\FormModel\Html;
+
+use BadMethodCallException;
+use InvalidArgumentException;
+use Traversable;
+use WScore\FormModel\Interfaces\BaseElementInterface;
+use WScore\FormModel\Interfaces\ElementInterface;
+use WScore\FormModel\Interfaces\FormElementInterface;
+use WScore\Html\Form;
+
+abstract class AbstractHtml implements HtmlFormInterface
+{
+    private $info = [];
+
+    /**
+     * @var HtmlFormInterface[]
+     */
+    private $children = [];
+
+    /**
+     * @param string $key
+     * @param string|mixed $default
+     * @return string|array|mixed
+     */
+    public function get($key, $default = '')
+    {
+        return $this->info[$key] ?? $default;
+    }
+
+    /**
+     * @return string
+     */
+    public function name()
+    {
+        return $this->get('name');
+    }
+
+    /**
+     * @return string
+     */
+    public function label()
+    {
+        return $this->get('label');
+    }
+
+    /**
+     * @return string
+     */
+    public function value()
+    {
+        return $this->get('value');
+    }
+
+    /**
+     * @return self[]
+     */
+    public function choices()
+    {
+        /** @var array $choices */
+        $choices = $this->get('choices', []);
+        if (empty($choices)) {
+            return [];
+        }
+        foreach ($choices as $key => $choice) {
+
+        }
+        return $choices;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasChildren(): bool
+    {
+        return !empty($this->children);
+    }
+
+    /**
+     * @return HtmlFormInterface[]
+     */
+    public function getChildren(): array
+    {
+        return $this->children;
+    }
+
+    /**
+     * @param string $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->children);
+    }
+
+    /**
+     * @param string $offset
+     * @return HtmlFormInterface
+     */
+    public function offsetGet($offset)
+    {
+        return $this->children[$offset] ?? null;
+    }
+
+    /**
+     * @param string $offset
+     * @param HtmlFormInterface $value
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        if ($value instanceof HtmlFormInterface) {
+            $this->children[$offset] = $value;
+            return;
+        }
+        throw new InvalidArgumentException();
+    }
+
+    /**
+     * @param string
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->children[$offset]);
+    }
+
+    /**
+     * @return Traversable|HtmlFormInterface[]
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->children);
+    }
+}
--- src/Html/Html.php
+++ src/Html/Html.php
@@ -3,36 +3,23 @@ namespace WScore\FormModel\Html;
 
 use Traversable;
 use WScore\FormModel\Interfaces\BaseElementInterface;
+use WScore\FormModel\Interfaces\ElementInterface;
+use WScore\FormModel\Interfaces\FormElementInterface;
+use WScore\Html\Form;
 
-class Html implements HtmlFormInterface
+class Html extends AbstractHtml
 {
+    /**
+     * @var BaseElementInterface|ElementInterface|FormElementInterface
+     */
+    private $element;
+
     public static function create(BaseElementInterface $element): HtmlFormInterface
     {
+        $self = new self();
+        $self->element = $element;
 
-    }
-
-    /**
-     * @return string
-     */
-    public function name()
-    {
-        // TODO: Implement name() method.
-    }
-
-    /**
-     * @return string
-     */
-    public function label()
-    {
-        // TODO: Implement label() method.
-    }
-
-    /**
-     * @return string
-     */
-    public function value()
-    {
-        // TODO: Implement value() method.
+        return $self;
     }
 
     /**
@@ -40,15 +27,13 @@ class Html implements HtmlFormInterface
      */
     public function form()
     {
-        // TODO: Implement form() method.
-    }
+        $type = $this->element->getType();
+        $name = $this->element->getFullName();
+        $value = $this->element->getValue();
+        $attributes = $this->element->getAttributes();
+        $form = Form::input($type, $name, $value)->setAttributes($attributes);
 
-    /**
-     * @return self[]
-     */
-    public function choices()
-    {
-        // TODO: Implement choices() method.
+        return $form;
     }
 
     /**
@@ -57,66 +42,5 @@ class Html implements HtmlFormInterface
     public function __toString()
     {
         // TODO: Implement __toString() method.
-    }
-
-    /**
-     * @return bool
-     */
-    public function hasChildren(): bool
-    {
-        // TODO: Implement hasChildren() method.
-    }
-
-    /**
-     * @return HtmlFormInterface[]
-     */
-    public function getChildren(): array
-    {
-        // TODO: Implement getChildren() method.
-    }
-
-    /**
-     * @param string $offset
-     * @return bool
-     */
-    public function offsetExists($offset)
-    {
-        // TODO: Implement offsetExists() method.
-    }
-
-    /**
-     * @param string $offset
-     * @return HtmlFormInterface
-     */
-    public function offsetGet($offset)
-    {
-        // TODO: Implement offsetGet() method.
-    }
-
-    /**
-     * @param string $offset
-     * @param HtmlFormInterface $value
-     * @return void
-     */
-    public function offsetSet($offset, $value)
-    {
-        // TODO: Implement offsetSet() method.
-    }
-
-    /**
-     * @param string
-     * @return void
-     */
-    public function offsetUnset($offset)
-    {
-        // TODO: Implement offsetUnset() method.
-    }
-
-    /**
-     * @return Traversable|HtmlFormInterface[]
-     */
-    public function getIterator()
-    {
-        // TODO: Implement getIterator() method.
     }
 }
