87ddf47c9cbb8dfd8b5092f0a08a0424fb976fb3
asaokamei <asao.kamei@gmail.com>
2019-05-26 10:49:00 +0900

    split a test into two tests.

---
+++ tests/FormBuilder/InputTypeTest.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use WScore\FormModel\FormBuilder;
+use WScore\FormModel\Interfaces\ToStringInterface;
+use WScore\FormModel\Validation\Validator;
+use WScore\Validation\Filters\StringCases;
+use WScore\Validation\Validators\Result;
+
+class InputTypeTest extends TestCase
+{
+    public function testFormModelText()
+    {
+        $fm = FormBuilder::create();
+        $text = $fm->text('name', 'User Name');
+        $text->setAttributes([
+            'class' => 'form-type',
+            'style' => 'width:5em',
+        ]);
+        $text->setFilters([
+            StringCases::class => [StringCases::TO_UPPER],
+        ]);
+        $html = $text->createHtml('test-me');
+        $this->assertEquals(
+            '<input type="text" name="name" id="name" value="test-me" class="form-type" style="width:5em" required="required">',
+            $html->form()->toString()
+        );
+        $this->assertEquals('User Name', $html->label());
+
+        $validator = $text->createValidation();
+        $this->assertEquals(Validator::class, get_class($validator));
+        $result = $validator->verify('my name');
+        $this->assertEquals(Result::class, get_class($result));
+        $this->assertEquals('MY NAME', $result->value());
+
+        $result = $validator->verify(null);
+        $this->assertFalse($result->isValid());
+        $this->assertSame('', $result->value());
+    }
+
+    public function testToString()
+    {
+        $fm = FormBuilder::create();
+        $text = $fm->text('name', 'User Name');
+        $html = $text->createHtml();
+        $toString = $html->toString();
+
+        $this->assertTrue($toString instanceof ToStringInterface);
+    }
+}
